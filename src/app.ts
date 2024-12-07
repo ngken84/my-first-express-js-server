65b0c01358417dd93251fa25723d4dcdf6706afc
Ken Ng <ng.ken84@gmail.com>
2024-12-06 19:23:44 -0800

    Implement Step 3 of assignment

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,12 @@ import express,{ Request, Response } from 'express';
 
 const app = express();
 
-
-app.use('/', (req: Request, res: Response, next: () => void) => {
-    console.log("First Middleware");
-    next();
+app.use('/users', (req: Request, res: Response, next: () => void) => {
+    res.send('<h1>Users</h1>')
 });
 
 app.use('/', (req: Request, res: Response, next: () => void) => {
-    console.log("Second Middleware");
-    res.send('<h1>One Response</h1>');
+    res.send('<h1>Home</h1>');
 });
 
 app.listen(3000);

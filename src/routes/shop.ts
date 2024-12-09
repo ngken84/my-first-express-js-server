809dfc0e4bd05173cf94c593f8442b889e4d7228
Ken Ng <ng.ken84@gmail.com>
2024-12-08 19:19:41 -0800

    Update use to get to ensure exact matching on home route

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -2,7 +2,7 @@ import express, { Request, Response} from 'express';
 
 const router = express.Router();
 
-router.use('/', (req: Request, res: Response, next: () => void) => {
+router.get('/', (req: Request, res: Response, next: () => void) => {
     res.send('<h1>Home</h1>');
 });
 

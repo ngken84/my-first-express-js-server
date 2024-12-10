78d8b2d118bc3fc90e77989d1a80ae87d3967fa3
Ken Ng <ng.ken84@gmail.com>
2024-12-10 11:23:07 -0800

    Add 404 page

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import path from 'path'
 
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req : Request, res: Response, next: () => void) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);

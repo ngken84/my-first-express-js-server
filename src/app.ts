7f8c24e78ba0edcbbe97ecd6fd7198e1f9cc6259
Ken Ng <ng.ken84@gmail.com>
2024-12-20 23:32:07 -0800

    Set up and start using EJS on 404 pages

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import path from 'path'
-import { engine } from 'express-handlebars';
+// import { engine } from 'express-handlebars';
 
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
@@ -11,9 +11,13 @@ const app = express();
 // app.set('view engine', 'pug');
 // app.set('views', path.join(__dirname, 'pug'));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, 'hbs'));
+// HANDLEBARS
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars')
+// app.set('views', path.join(__dirname, 'hbs'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'ejs'));
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,7 +27,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req : Request, res: Response, next: () => void) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: '404'});
 });
 
 app.listen(3000);
